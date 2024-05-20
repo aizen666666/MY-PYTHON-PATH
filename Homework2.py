ff102cbf754e33fa07cdf17d45912bc8e7a859ca
aizen666666 <151596754+aizen666666@users.noreply.github.com>
2024-05-20 20:52:28 +0500

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,6 @@
+les = 12
+hours = 1.5
+name = "Python"
+avg_time=hours/les
+print("Курс:", name,"," "Всего задач:", les,"," "Всего затраченно часов:", hours, "," "среднее время выполнения", avg_time,"часа.")
+
