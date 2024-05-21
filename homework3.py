f0ba3ec3e598cc058bdbe92eca9cbaa67f0bba13
aizen666666 <151596754+aizen666666@users.noreply.github.com>
2024-05-21 16:53:31 +0500

    Update homework3 edited.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,7 @@
 name = "Misha"
-age = 22
-new_age = age+1
+age = 22
 is_student=True
 print('Name:',name)
 print('age:', age)
-print('New age:',new_age)
+print('New age:',age+1)
 print('Is student:',is_student)
